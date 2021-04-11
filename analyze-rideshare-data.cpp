099cc574e9f06537a6131fa6781585d6b05872b3
Kristoffer Semelka <ksemelka@gmail.com>
2021-04-10 18:40:25 -0700

    just some test data

---
+++ analyze-rideshare-data.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+
+#include "cab-rides-row.h"
+#include "weather-row.h"
+
+using namespace std;
+
+vector<WeatherRow> getRideTestData() {
+    vector<CabRidesRow> rows = {
+        // distance, cab_type, time_stamp, price, product_name
+        // price = (distance + noise(mean=1, std=0.4)) * 2
+        { 10.0, CabType::Lyft, 7, 21.83070, "normal" },
+        { 10.0, CabType::Lyft, 10, 23.77734, "normal" },
+        { 10.0, CabType::Lyft, 5, 21.98066, "normal" },
+        { 10.0, CabType::Lyft, 11, 22.35094, "normal" },
+        { 11.0, CabType::Lyft, 6, 25.34288, "normal" },
+        { 11.0, CabType::Lyft, 9, 24.54997, "normal" },
+        { 12.0, CabType::Lyft, 2, 26.77077, "normal" },
+        { 12.0, CabType::Lyft, 1, 25.85557, "normal" },
+        { 13.0, CabType::Lyft, 4, 26.64197, "normal" },
+        { 14.0, CabType::Lyft, 8, 28.79596, "normal" },
+        { 15.0, CabType::Lyft, 14, 31.49976, "normal" },
+        { 16.0, CabType::Lyft, 12, 34.10198, "normal" },
+        { 17.0, CabType::Lyft, 3, 34.53185, "normal" },
+        { 18.0, CabType::Lyft, 0, 38.73401, "normal" },
+        { 19.0, CabType::Lyft, 13, 39.70895, "normal" },
+
+        // price = (distance + noise(mean=1, std=0.4)) * 1.5
+        { 10.0, CabType::Uber, 13, 17.19143, "normal" },
+        { 10.0, CabType::Uber, 12, 15.51235, "normal" },
+        { 10.0, CabType::Uber, 7, 15.54780, "normal" },
+        { 10.0, CabType::Uber, 2, 17.81076, "normal" },
+        { 11.0, CabType::Uber, 14, 18.39082, "normal" },
+        { 11.0, CabType::Uber, 3, 18.01110, "normal" },
+        { 12.0, CabType::Uber, 4, 20.85211, "normal" },
+        { 12.0, CabType::Uber, 10, 20.57887, "normal" },
+        { 13.0, CabType::Uber, 9, 21.94923, "normal" },
+        { 14.0, CabType::Uber, 0, 23.16039, "normal" },
+        { 15.0, CabType::Uber, 11, 22.99939, "normal" },
+        { 16.0, CabType::Uber, 1, 25.79663, "normal" },
+        { 17.0, CabType::Uber, 8, 27.18243, "normal" },
+        { 18.0, CabType::Uber, 6, 27.28597, "normal" },
+        { 19.0, CabType::Uber, 5, 31.01332, "normal" }
+    };
+}
+
+vector<WeatherRow> getWeatherTestData() {
+    vector<WeatherRow> rows = {
+        // it was hot... then it was cold
+        {}
+    };
+}
+
+int main() {
+    return 0;
+}
