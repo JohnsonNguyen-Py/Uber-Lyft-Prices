b30ac8941e0fd7fa4666aedd7b94bb19b6eb8f14
jonathanko500 <jkko9010@gmail.com>
2021-04-11 20:31:36 -0700

    place data value into specific vectors
    made vectors that are specific to data we need. made all vectors accept string vectors due to not knowing how to put a string variable into a non-string vector

--- cab-rides-row.h
+++ cab-rides-row.h
@@ -2,23 +2,37 @@
 #define CAB_RIDES_ROW_H
 
 #include <string>
+#include <vector>
 
 using namespace std;
-
-enum CabType 
-{
-    Lyft, Uber
-};
 
 class CabRidesRow
 {
 public:
-    float distance;
-    CabType cab_type;
-    int time_stamp;
-    float price;
-    string product_name;
-    void vectorDataInsert();
+    //travel distances ride
+    //float
+    vector <string> distance;
+
+    //prices of service
+    //float
+    vector <string> price;
+
+    //time stamp of each ride 
+    //int
+    vector <string> time_stamp;
+
+    //specific product purchased
+    vector <string> product_name;
+
+    //uber vs lyft
+    vector <string> cab_type;
+
+    //every other uneeded data pt
+    vector <string> misc;
+    //function to insert data from csv file 
+
+    void vectorRideDataInsert();
+
 };
 
 #endif
--- weather-row.h
+++ weather-row.h
@@ -1,15 +1,31 @@
 #ifndef WEATHER_ROW_H
 #define WEATHER_ROW_H
 
+#include <string>
+#include <vector>
+
 using namespace std;
 
 class WeatherRow
 {
 public:
-    float temp;
-    float rain;
-    int time_stamp;
-    void vectorDataInsert();
+    //travel distances ride
+    //float
+    vector <string> temp;
+
+    //prices of service
+    //float
+    vector <string> rain;
+
+    //time stamp of each ride 
+    //int
+    vector <string> time_stamp;
+
+    //every other uneeded data pt
+    vector <string> misc;
+    //function to insert data from csv file 
+
+    void vectorWeatherDataInsert();
 };
 
 #endif
